fe2c2bb7a45dc25534fa61db33af2a5184607aa1
Jeremy Clark <jeremy@jeremybytes.com>
2020-01-24 23:43:00 -0800

    Added Dynamic Loading using Plugin architecture

---
+++ completed/04-DynamicLoading-Plugin/PersonReader.SQL/SQLReader.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonReader.Interface;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonReader.SQL
+{
+    public class SQLReader : IPersonReader
+    {
+        DbContextOptions<PersonContext> options;
+
+        public SQLReader()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PersonContext>();
+            optionsBuilder.UseSqlite("Data Source=People.db");
+            options = optionsBuilder.Options;
+        }
+
+        public IReadOnlyCollection<Person> GetPeople()
+        {
+            using (var context = new PersonContext(options))
+            {
+                return context.People.ToList();
+            }
+        }
+
+        public Person GetPerson(int id)
+        {
+            using (var context = new PersonContext(options))
+            {
+                return context.People.Where(p => p.Id == id).FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ starter/04-DynamicLoading-Plugin/PeopleViewer/ReaderFactory.cs
@@ -0,0 +1,36 @@
+﻿using PersonReader.Interface;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace PeopleViewer
+{
+    public static class ReaderFactory
+    {
+        private static IPersonReader reader;
+
+        public static IPersonReader GetReader()
+        {
+            if (reader != null)
+                return reader;
+
+            string readerAssemblyName = ConfigurationManager.AppSettings["ReaderAssembly"];
+            string readerLocation = AppDomain.CurrentDomain.BaseDirectory
+                                    + "ReaderAssemblies"
+                                    + Path.DirectorySeparatorChar
+                                    + readerAssemblyName;
+
+            ReaderLoadContext loadContext = new ReaderLoadContext(readerLocation);
+            AssemblyName assemblyName = new AssemblyName(Path.GetFileNameWithoutExtension(readerLocation));
+            Assembly readerAssembly = loadContext.LoadFromAssemblyName(assemblyName);
+
+            string readerTypeName = ConfigurationManager.AppSettings["ReaderType"];
+            Type readerType = readerAssembly.ExportedTypes
+                                .FirstOrDefault(t => t.FullName == readerTypeName);
+            reader = Activator.CreateInstance(readerType) as IPersonReader;
+            return reader;
+        }
+    }
+}
